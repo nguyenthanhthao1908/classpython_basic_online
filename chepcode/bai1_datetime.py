f616e8d748efd29b5a60ebf76bde0ed2294ef0b7
nguyenthanhthao1908 <63413170+nguyenthanhthao1908@users.noreply.github.com>
2020-04-21 01:47:49 +0700

    Update bai1_datetime.py

--- chepcode/bai1_datetime.py
+++ chepcode/bai1_datetime.py
@@ -1,14 +1,14 @@
 
-from datetime import datetime       #from datetime from module datetime
+from datetime import datetime       #import module datetime from datetime
 import pytz                         #truy cap thu vien pytz
 
 local = datetime.now()              #tra ve ngay-gio hien tai
-print("local:", local.strftime("%m/%d/%Y, %H:%M:%S"))       #in ra man hinh ngay-gio
+print("local:", local.strftime("%m/%d/%Y, %H:%M:%S"))       #in ra man hinh ngay-gio, phuong thuc str
 
 tz_NY = pytz.timezone('America/New_York')       #chuyen doi mui gio, Time zone argument is New_York
 datetime_NY = datetime.now(tz_NY)           #tra ve ngay-gio hien tai cua New_york
-print("NY:", datetime_NY.strftime("%m/%d/%Y, %H:%M:%S"))        #in ra man hinh ngay-gio
+print("NY:", datetime_NY.strftime("%m/%d/%Y, %H:%M:%S"))        #in ra man hinh ngay-gio, phuong thuc str
 
 tz_London = pytz.timezone('Europe/London')      #chuyen doi mui gio, Time zone argument is London
 datetime_London = datetime_NY.now(tz_London)       #tra ve ngay-gio hien tai cua New_york
-print("London:", datetime_London.strftime("%m/%d/%Y, %H:%M:%S"))    #in ra man hinh ngay-gio
+print("London:", datetime_London.strftime("%m/%d/%Y, %H:%M:%S"))    #in ra man hinh ngay-gio, phuong thuc str
