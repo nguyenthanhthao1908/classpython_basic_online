cacf360af9a15cb9e6b404781592d68c0d216f8d
nguyenthanhthao1908 <63413170+nguyenthanhthao1908@users.noreply.github.com>
2020-04-22 02:56:30 +0700

    Delete baiso1.py

--- baiso1.py
+++
@@ -1,8 +0,0 @@
-"""Bài 01:
-    Viết chương trình thay thế tất cả các ký tự giống ký tự đầu tiên của một Chuỗi thành $.
-"""
-S1 = input("Enter a string: ")
-for i in S1:
-    if i == S1[0]:
-        print("String received is: ", S1.replace(i, "$"))
-        break
