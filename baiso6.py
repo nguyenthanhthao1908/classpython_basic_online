cee1b20982ccd2296e89488f43b5b1b536e39e73
nguyenthanhthao1908 <63413170+nguyenthanhthao1908@users.noreply.github.com>
2020-04-22 02:57:12 +0700

    Delete baiso6.py

--- baiso6.py
+++
@@ -1,5 +0,0 @@
-"""Bài 06:
- Viết chương trình đảo ngược từ ký tự thường sang ký tự hoa và từ ký tự hoa sang ký tự thường trong một chuỗi.
-"""
-S6 = input("Enter a string:")
-print("String received is:", S6.swapcase())
