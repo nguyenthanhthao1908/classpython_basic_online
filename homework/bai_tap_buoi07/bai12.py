fbb8ff7f2e04c6f7feca8e336e0bb194b5751329
nguyenthanhthao1908 <63413170+nguyenthanhthao1908@users.noreply.github.com>
2020-05-19 20:30:16 +0700

    Update bai12.py

--- homework/bai_tap_buoi07/bai12.py
+++ homework/bai_tap_buoi07/bai12.py
@@ -7,7 +7,7 @@ def find_x(a_list, x):
     for i in range(len(a_list)):
         if x == a_list[i]:
             index.append(i)
-    if len(index) == 0:
+    if x not in a_list:
         return -1
     return index
 print(find_x([3, 45, 3, 5, 45, 3, 6],3))
